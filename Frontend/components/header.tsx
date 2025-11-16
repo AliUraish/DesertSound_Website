f66d5cb33555e27d62011225ded412f647aee9d5
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-11-16 02:00:01 -0500

    increased the width of the container and header of the hero section

--- Frontend/components/header.tsx
+++ Frontend/components/header.tsx
@@ -10,7 +10,7 @@ export function Header() {
   return (
     <>
       <header className="fixed top-0 left-0 right-0 z-50 bg-background">
-        <div className="max-w-7xl mx-auto px-6 lg:px-8">
+        <div className="max-w-[90%] mx-auto px-3 lg:px-4">
           <div className="relative flex items-center justify-between py-4 lg:py-5">
             {/* Logo - Left */}
             <div className="flex items-center cursor-pointer">
--- Frontend/components/hero-section.tsx
+++ Frontend/components/hero-section.tsx
@@ -37,7 +37,7 @@ export function HeroSection() {
 
   return (
     <section className="relative min-h-screen w-full bg-background">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8 pt-20 lg:pt-24">
+      <div className="max-w-[90%] mx-auto px-3 lg:px-4 pt-20 lg:pt-24">
         {/* Image Container */}
         <div className="relative w-full h-[calc(100vh-8rem)] lg:h-[calc(100vh-10rem)] rounded-lg overflow-hidden">
           {heroSlides.map((slide, index) => (
