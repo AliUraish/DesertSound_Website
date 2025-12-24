831046fdd76047d324e67e972755442e2c7466f7
Abdul Rahim Mirani <abdulrahimmirani@gmail.com>
2025-12-24 02:59:41 -0500

    moved the hero section a bit down

--- Frontend/components/hero-section.tsx
+++ Frontend/components/hero-section.tsx
@@ -42,7 +42,7 @@ export function HeroSection() {
 
   return (
     <section className="relative min-h-screen w-full bg-[#F5F5DC]">
-      <div className="max-w-[90%] mx-auto px-3 lg:px-4 pt-20 lg:pt-24">
+      <div className="max-w-[90%] mx-auto px-3 lg:px-4 pt-24 lg:pt-28">
         {/* Image Container */}
         <div className="relative w-full h-[calc(100vh-8rem)] lg:h-[calc(100vh-10rem)] rounded-lg overflow-hidden">
           {heroSlides.map((slide, index) => (
